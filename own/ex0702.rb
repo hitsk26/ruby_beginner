7298b345750cba60324f7446aec84ee709908bc8
hitsk26 <hitsk26@gmail.com>
2013-04-05 20:33:37 +0900

    playing string litteral

---
+++ own/ex0702.rb
@@ -0,0 +1,19 @@
+
+
+puts 'h"ge'
+puts "fu'ga"
+
+
+str = String.new("foo\n")
+
+str2 = "annother msg
+and hoge"
+
+str3 = str + str2
+
+print str
+
+print "str2=" + str2
+print str3 + "\n"
+
+puts "youkos" "miraie"
