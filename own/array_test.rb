02d0f6c430722ea34e4ca36e2557275be83f8263
hitsk26 <hitsk26@gmail.com>
2013-04-08 16:31:47 +0900

    added array_test and [] method

---
+++ own/array_test.rb
@@ -0,0 +1,9 @@
+train_types = ["local","rapid","express"]
+exam_scores = [40,35,22,70,90]
+fruits_prices =[["apple",200],["orange",100],["mellon",450000]]
+
+puts train_types
+
+#puts train_types[2]
+print fruits_prices[1]#[1]
+#puts fruits_prices[2][0]
---
+++ own/dataclass_test.rb
@@ -0,0 +1,8 @@
+require 'date'
+
+day = Date.today
+
+print  day.to_s + "\n"
+
+hinamaturi = Date.new(2013,3,3)
+puts "hinamaturi" + hinamaturi.to_s 
