a1e74d175442af527d99c09c8637ffd3372dc387
hitsk26 <hitsk26@gmail.com>
2013-05-05 08:54:49 +0900

    implemented print book info by first method using arrray

--- own/ex0806.rb
+++ own/ex0806.rb
@@ -15,3 +15,22 @@ sizes = ["21 x 14.8 x 2","23 x 18.2 x 1.6"]
 publish_dates = [Date.new(2005,1,25),Date.new(2006,12,23)]
 purchase_dates = [Date.new(2005,2,2), Date.new(2007,1,10)]
 
+
+#print libraries
+
+titles.size.times { |i|
+  puts "-------------------"
+  puts "title:" + titles[i]
+  puts "author" + authors[i]
+  puts "yomigana" + yomies[i]
+  puts "publisher" + publishers[i]
+  puts "pages" + pages[i].to_s + "ページ"
+  puts "販売価格" + prices[i].to_s + "yen"
+  puts "購入費用" + purchase_prices[i].to_s + "yen"
+  puts "ISNB_10" + isbn_10s[i]
+  puts "ISNB_13" + isbn_13s[i]
+  puts "size" + sizes[i]
+  puts "publish date"  + publish_dates[i].to_s
+  puts "purchase date" + purchase_dates[i].to_s
+}
+
