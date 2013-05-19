6fdbd26615edff061ae84f67c492077649f2331a
hitsk26 <hitsk26@gmail.com>
2013-05-19 17:32:38 +0900

    added original geter setter to student.rb

--- own/student.rb
+++ own/student.rb
@@ -11,6 +11,23 @@ class Student
     str = "#{@name},#{@age}, #{@title},#{@price}"
     return str
   end
+
+  def name
+    @name
+  end
+
+  def age
+    @age
+  end
+
+  def name=(value)
+    @name = value
+  end
+
+  def age=(value)
+    @age = value
+  end
+
 end
 
 shin = Student.new "shin",20,"agile begiener",2500
@@ -18,3 +35,9 @@ kubo = Student.new("kubosan",3,"lego hoge",50)
 
 puts shin.to_s
 puts kubo.to_s
+
+puts "name:#{shin.name} age:#{shin.age}"
+
+shin.name= ("hogeshin")
+
+puts shin.to_s
